4bbe3f4b1378b63b34b1b8e64c217c22b8a26b57
tuncayofficial <1280300@student.edu.az>
2022-07-23 15:29:08 +0400

    Header Files added!

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo Enter destination directory for your C project (type 'def' for home directory):
+echo Enter destination directory for your C project \(type 'def' for home directory\):
 
 read DIR
 
-if[$dir = "def"]
-then
+if [$DIR = "def"]; then
     DIR=$HOME
 fi
 
-cp -i ./src/* $DIR
+git clone https://github.com/tuncayofficial/cprojmaker/tree/master/src
---
+++ src/HEADER_FILES/header.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\nThis is the start of your C project header file!.\n");
+    return 0;
+}
